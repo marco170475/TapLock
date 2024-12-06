d068c6bf87c63a35e0348ac3e313a6dbd8a5c93c
modelorona <angel.hristozov@gmail.com>
2024-12-06 18:54:09 +0000

    add screenshots

--- app/src/main/java/com/ah/taplock/ui/theme/Color.kt
+++ app/src/main/java/com/ah/taplock/ui/theme/Color.kt
@@ -4,7 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 // dark scheme colors
 val MidnightGreen = Color(0xFF1A535C)
-val Cream = Color(0xFFF5FDC6)
 val CarolinaBlue = Color(0xFF73B3CE)
 val MountbattenPink = Color(0xFF946E83)
-val BurntSienna = Color(0xFFDD6E42)
